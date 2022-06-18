f1bfd7aad5593f1480caa3ece955b8634fd838b6
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-06-18 12:51:04 -0700

    fixed seeds issue

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST_LOG,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,7 +13,7 @@ VALUES
 ("Engineer Manager", 150000, 2), 
 ("Software Engineer", 120000, 2), 
 ("CFO", 130000, 3),
-("Accountant", 130000, 3),
+("Accountant", 70000, 3),
 ("Sous Chef", 90000, 4), 
 ("Executive Chef", 120000, 4),  
 ("Lawyer", 190000, 5), 
@@ -21,10 +21,14 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-("Philip", "Hwang", 3, NULL), 
-("Samy", "Tann", 4, 1), 
-("Debbi", "U", 6, NULL), 
-("Neo", "Du", 2, NULL), 
-("Keeler", "Cowie", 1, 4),
-("Nick", "Choi", 5, 2);
+("Diana", "Nguyen", 6, 4), 
+("Philip", "Hwang", 2, NULL),  
+("David", "Bird", 1, 2), 
+("Ivy", "Ha", 5, NULL), 
+("Samy", "Tann", 9, 7), 
+("Debbi", "U", 3, NULL), 
+("Neo", "Du", 10, NULL), 
+("Keeler", "Cowie", 7, 10),
+("Nick", "Choi", 4, 6),
+("Viet", "Nguyen", 8, NULL);
 
